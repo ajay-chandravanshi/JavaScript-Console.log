8e6e50924ae4250434b5750f6ea67a71eab1eeb4
ajay-chandravanshi <ajaystudy38@gmail.com>
2024-12-19 15:00:51 +0530

    first commit

---
+++ console.js
@@ -0,0 +1,6 @@
+console.log("Hello Sir");
+console.log("What is your name");
+console.log("Do You Know My Name");
+console.log("I Am Ajay Chandravanshi");
+console.log("I Think You Are Fine");
+console.log("Nice To Meet You Good Day Bye");
